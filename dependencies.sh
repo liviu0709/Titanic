c69b6d780ede6e3a0ada67ac0feb2d2464d40679
Stoica Liviu <liviu.stoica0709@stud.acs.upb.ro>
2024-05-26 20:23:31 +0300

    Added dependencies script

---
+++ dependencies.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt install -y graphviz
+
+pip install PyQt5
+pip install scipy
+pip install scikit-learn
+pip install pandas
+pip install matplotlib
+pip install seaborn
+pip install numpy
+pip install pydotplus
+pip install ipython
