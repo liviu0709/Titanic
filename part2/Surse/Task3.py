38cfe594a5fe6e68b6247d88277361abb31f5afc
Stoica Liviu <liviu.stoica0709@stud.acs.upb.ro>
2024-05-28 21:22:27 +0300

    Fixed paths for data_read

--- part2/Surse/Task3.py
+++ part2/Surse/Task3.py
@@ -2,14 +2,13 @@ import pandas as pd
 
 import sys
 sys.path.append("../../")
-import data_read
+import part1.surse.data_read
 
 data = pd.read_csv("../Date/noOutliersModel.csv")
 
 # Compare output data with the one from Part 1
-data_read.survival_percentage(data)
-data_read.histograms(data)
-data_read.null_statistics(data)
-data_read.correlation(data)
+part1.surse.data_read.survival_percentage(data, True)
+part1.surse.data_read.histograms(data, "Age", True)
+part1.surse.data_read.correlation(data, True)
 
 # After comparing them, they look similar, but the data from Part 1 has no outliers, so the data is more accurate.
